860292887dc638a5bbc617fcf8d2aabf4b524223
Jose Luis Torrentera <jltorrenterash@gmail.com>
2020-10-12 20:50:25 -0500

    Login Page View

---
+++ lib/login_page.dart
@@ -0,0 +1,82 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({Key key}) : super(key: key);
+  
+
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  Color iconColor = Colors.grey;
+
+  bool obscureText = true;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Inicio de Sesión"),
+        
+      ),
+      body: SingleChildScrollView(
+          child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(70.0),
+              child: Container(
+                child: Text("RoomMe", style: TextStyle(fontSize: 50, fontWeight: FontWeight.bold),)
+              ),
+            ),
+            SizedBox(height: 40),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20),
+              child: TextField(
+                decoration: InputDecoration(
+                  labelText: "Username",
+                  filled: true,
+                ),
+              ),
+            ),
+            SizedBox(height: 40,),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20),
+              child: TextField( 
+                decoration: InputDecoration(
+                  filled: true,
+                  labelText: "Password",
+                  suffixIcon: IconButton(
+                        onPressed: () {
+                          setState(() {
+                            obscureText = !obscureText;
+                            if(obscureText) {
+                              iconColor = Colors.grey;
+                            }else {
+                              iconColor = Colors.black;
+                            }
+                          });
+                        },
+                        icon: Icon(Icons.remove_red_eye, color: iconColor)
+                      )  
+                    ),
+                obscureText: obscureText,
+              ),
+                
+                
+              ),
+            
+            SizedBox(height: 80,),
+            RaisedButton(
+              child: Text("INICIAR SESIÓN"),
+              color: Color(0xFFFEDCD2),
+              onPressed: (){}
+            ),
+            SizedBox(height: 30,),
+            Text("Crear una cuenta", style: TextStyle(color: Colors.lightBlue, decoration: TextDecoration.underline),)
+          ]
+        ),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+
+import 'login_page.dart';
  
 void main() => runApp(MyApp());
  
@@ -6,20 +8,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Material App',
+      title: 'Room Me',
       theme: ThemeData(
         primaryColor: Color(0xFF8FD8D2)
       ),
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Material App Bar'),
-        ),
-        body: Center(
-          child: Container(
-            child: Text('Hello World'),
-          ),
-        ),
-      ),
+      home: LoginPage(),
     );
   }
 }
