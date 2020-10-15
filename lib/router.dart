e9786eea59b3481150587084ccabf4e6bdfa9cc0
Carlos Soto Pérez <carlos348@outlook.com>
2020-10-15 11:53:58 -0500

    Separación de rutas y App main

---
+++ lib/router.dart
@@ -0,0 +1,47 @@
+import 'package:RoomMeMobile/house/create_house.dart';
+import 'package:RoomMeMobile/house/details_house.dart';
+import 'package:RoomMeMobile/login.dart';
+import 'package:RoomMeMobile/register.dart';
+import 'package:RoomMeMobile/usuario/user.dart';
+import 'package:flutter/material.dart';
+
+Route<dynamic> buildRouter(RouteSettings settings) {
+  switch (settings.name) {
+    case '/login':
+      return MaterialPageRoute<void>(
+        settings: settings,
+        builder: (BuildContext context) => Login(),
+        fullscreenDialog: true,
+      );
+    case '/register':
+      return MaterialPageRoute<void>(
+        settings: settings,
+        builder: (BuildContext context) => Register(),
+        fullscreenDialog: true,
+      );
+    case '/user':
+      return MaterialPageRoute<void>(
+        settings: settings,
+        builder: (BuildContext context) => UserPage(),
+        fullscreenDialog: true,
+      );
+    case '/house/detail':
+      return MaterialPageRoute<void>(
+        settings: settings,
+        builder: (BuildContext context) => DetailsHouse(),
+        fullscreenDialog: true,
+      );
+    case '/house/new':
+      return MaterialPageRoute<void>(
+        settings: settings,
+        builder: (BuildContext context) => CreateHouse(),
+        fullscreenDialog: true,
+      );
+    default:
+      return MaterialPageRoute(
+          builder: (_) => Scaffold(
+                body: Center(
+                    child: Text('No route defined for ${settings.name}')),
+              ));
+  }
+}
