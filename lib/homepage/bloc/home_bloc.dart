6f085d1ce2acfa87c194c11a34ca4b82260cdf3c
Juan Tec <reluro1@gmail.com>
2020-11-16 19:26:22 -0600

    Started houses request implementation

--- lib/homepage/bloc/home_bloc.dart
+++ lib/homepage/bloc/home_bloc.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 import 'dart:convert';
 
+import 'package:RoomMeMobile/http/client.dart';
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
 import 'package:flutter/foundation.dart';
@@ -11,7 +12,7 @@ part 'home_event.dart';
 part 'home_state.dart';
 
 class HomeBloc extends Bloc<HomeEvent, HomeState> {
-  final _link = "https://room-me-app.herokuapp.com/house";
+  final _link = "https://room-me-app.herokuapp.com/house/all";
 
   HomeBloc() : super(InitialState(body: ''));
 
@@ -19,22 +20,23 @@ class HomeBloc extends Bloc<HomeEvent, HomeState> {
   Stream<HomeState> mapEventToState(
     HomeEvent event,
   ) async* {
-    if (event is InitialEvent) {
-      // String response = await _getHouses();
+    if (event is InitialEvent){
+      dynamic houses = _getHouses();
+      print('houses:' + houses.toString());
       yield InitialState(body: {'foto': 'https://q4g9y5a8.rocketcdn.me/wp-content/uploads/2020/02/home-banner-2020-02-min.jpg', 'title': 'Casa de Maki'});
     }
-  //     else
-  //       yield ErrorState(error: response);
-  //   }
   }
 
-  Future<String> _getHouses() async {
+  dynamic _getHouses() {
     try {
-      Response response = await get(_link, headers:{"Content-type": "application/json"});
-      if(response.statusCode == 200)
-        return response.body;
-      else
-        return jsonDecode(response.body)['error'];
+      HttpClient client = HttpClient.getClient();
+      dynamic response = client.get(_link, null);
+      
+      return response;
+      // if(response.statusCode == 200)
+      //   return response.body;
+      // else
+      //   return jsonDecode(response.body)['error'];
     } catch (error) {
       print(error.toString());
       return (error.toString());
