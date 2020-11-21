28eb44da48cb7d3b16cf8e586e22583b9f3b6ed1
Juan Tec <reluro1@gmail.com>
2020-11-20 18:14:00 -0600

    Implemented house members and tasks handler

--- lib/homepage/house_item.dart
+++ lib/homepage/house_item.dart
@@ -5,15 +5,13 @@ class HouseItem extends StatefulWidget {
 
   final double cardHeight;
   final double cardWidth;
-  final String url;
-  final String houseName;
+  final dynamic house;
 
   HouseItem({
     Key key,
     @required this.cardHeight,
     @required this.cardWidth,
-    @required this.url,
-    @required this.houseName
+    @required this.house
   }) : super(key: key);
 
   @override
@@ -38,7 +36,7 @@ class _HouseItemState extends State<HouseItem> {
         child: Column(
         children:[
           Image.network(
-            widget.url,
+            widget.house['foto'],
             fit: BoxFit.fill,
             height: widget.cardHeight/4*3,
             width: widget.cardWidth
@@ -52,13 +50,67 @@ class _HouseItemState extends State<HouseItem> {
                   padding: EdgeInsets.only(left: 10),
                   child: Container(
                     width: widget.cardWidth - 160,
-                    child: Text(widget.houseName, overflow: TextOverflow.ellipsis, style: TextStyle(fontSize: 15, fontWeight: FontWeight.w500))
+                    child: Text(widget.house['title'], overflow: TextOverflow.ellipsis, style: TextStyle(fontSize: 15, fontWeight: FontWeight.w500))
                   )
                 ),
                 Row(
                   children: [
-                    IconButton(icon: Icon(Icons.content_paste), onPressed: (){}),
-                    IconButton(icon: Icon(Icons.people), onPressed: (){}),  
+                    IconButton(icon: Icon(Icons.content_paste), onPressed: (){
+                      showDialog(
+                        context: context,
+                        builder: (context) {
+                          return AlertDialog(
+                            title: Text('Tareas'),
+                            content: Container(
+                              height: width/2,
+                              width: width/5*3,
+                              child: widget.house['tasks'].length > 0 ? ListView.builder(
+                                itemCount: widget.house['tasks'].length,
+                                itemBuilder: (BuildContext context, int index) {
+                                  return Row(
+                                    children: [
+                                      Text(widget.house['tasks'][index]['description']),
+                                      widget.house['tasks'][index]['complete'] ? Icon(Icons.check) : Container()
+                                    ]
+                                  );
+                                }
+                              ) : Center( child: Text('Sin tareas por hacer'))
+                            ),
+                            actions: [
+                              FlatButton(onPressed: (){ Navigator.of(context).pop();}, child: Text('OK'))
+                            ]
+                          );
+                        }
+                      );
+                    }),
+                    IconButton(icon: Icon(Icons.people), onPressed: (){
+                      showDialog(
+                        context: context,
+                        builder: (context) {
+                          return AlertDialog(
+                            title: Text('Miembros'),
+                            content: Container(
+                              height: width/2,
+                              width: width/5*3,
+                              child: ListView.builder(
+                                itemCount: widget.house['members'].length,
+                                itemBuilder: (BuildContext context, int index) {
+                                  return Row(
+                                    children: [
+                                      Icon(Icons.person),
+                                      Text(widget.house['members'][index]['name'] + ' ' + widget.house['members'][index]['lastName'])
+                                    ]
+                                  );
+                                }
+                              )
+                            ),
+                            actions: [
+                              FlatButton(onPressed: (){ Navigator.of(context).pop();}, child: Text('OK'))
+                            ]
+                          );
+                        }
+                      );
+                    }),
                     IconButton(
                       icon: Icon(Icons.chat_bubble, color: Colors.black),
                       onPressed: () {
