d08343a9321b400c97f66de41a1d333685b6c215
Carlos Soto Pérez <carlos348@outlook.com>
2020-11-27 20:31:58 -0600

    Posible solución en el text field y Leading en homepage

--- lib/homepage/home.dart
+++ lib/homepage/home.dart
@@ -5,8 +5,6 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'bloc/home_bloc.dart';
 
 class Home extends StatefulWidget {
-
-
   Home({Key key}) : super(key: key);
 
   @override
@@ -25,8 +23,7 @@ class _HomeState extends State<Home> {
       Navigator.of(context)
           .pushNamed('/house/detail', arguments: hid)
           .then((value) {
-        BlocProvider.of<HomeBloc>(context)
-            .add(InitialEvent());
+        BlocProvider.of<HomeBloc>(context).add(InitialEvent());
       });
     }
 
@@ -38,66 +35,65 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     double width = MediaQuery.of(context).size.width;
-    final double cardWidth = width*0.8;
-    final double cardHeight = (cardWidth/16)*9;
+    final double cardWidth = width * 0.8;
+    final double cardHeight = (cardWidth / 16) * 9;
     List houses = new List();
 
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Mis casas'),
-        actions: [
-          IconButton(icon: Icon(Icons.person), onPressed: (){
-            Navigator.of(context).pushNamed('/user/me');
-          }),
-          IconButton(icon: Icon(Icons.pie_chart), onPressed: () async {
-            bool changes = await Navigator.of(context).pushNamed('/dashboard', arguments: houses);
-            if (changes)
+        appBar: AppBar(
+          title: Text('Mis casas'),
+          automaticallyImplyLeading: false,
+          actions: [
+            IconButton(
+                icon: Icon(Icons.person),
+                onPressed: () {
+                  Navigator.of(context).pushNamed('/user/me');
+                }),
+            IconButton(
+                icon: Icon(Icons.pie_chart),
+                onPressed: () async {
+                  bool changes = await Navigator.of(context)
+                      .pushNamed('/dashboard', arguments: houses);
+                  if (changes) bloc.add(InitialEvent());
+                })
+          ],
+        ),
+        body: BlocProvider(
+            create: (context) {
+              bloc = HomeBloc();
               bloc.add(InitialEvent());
-          })
-        ],
-      ),
-      body: BlocProvider(
-        create: (context) {
-          bloc = HomeBloc();
-          bloc.add(InitialEvent());
-          return bloc;
-        },
-        child: BlocConsumer<HomeBloc, HomeState>(
-          listener: (context, state) {
-            if (state is ErrorState) {
-              Scaffold.of(context)
-                ..hideCurrentSnackBar()
-                ..showSnackBar(
-                  SnackBar(content: Text("Error: ${state.error}")),
-                );
-            }
-          },
-          builder: (context, state) {
-            if (state is InitialState){
-              houses = state.body;
-              if(state.body.length > 0)
-                return RefreshIndicator(
-                  child: ListView.builder(
-                    padding: EdgeInsets.all((width - cardWidth)/2),
-                    itemCount: state.body.length,
-                    itemBuilder: (BuildContext context, int index) {
-                      return HouseItem(
-                        onTapHouse: onTapHouse(context),
-                        cardWidth: cardWidth,
-                        cardHeight: cardHeight,
-                        house: state.body[index]);
-                    }
-                  ),
-                  onRefresh: () async {
-                    bloc.add(InitialEvent());
-                  }
-                );
-              else
-                return Center(child: Text('Sin casas disponibles'));
-            }
-          }
-        )
-      )
-    );
+              return bloc;
+            },
+            child:
+                BlocConsumer<HomeBloc, HomeState>(listener: (context, state) {
+              if (state is ErrorState) {
+                Scaffold.of(context)
+                  ..hideCurrentSnackBar()
+                  ..showSnackBar(
+                    SnackBar(content: Text("Error: ${state.error}")),
+                  );
+              }
+            }, builder: (context, state) {
+              if (state is InitialState) {
+                houses = state.body;
+                if (state.body.length > 0)
+                  return RefreshIndicator(
+                      child: ListView.builder(
+                          padding: EdgeInsets.all((width - cardWidth) / 2),
+                          itemCount: state.body.length,
+                          itemBuilder: (BuildContext context, int index) {
+                            return HouseItem(
+                                onTapHouse: onTapHouse(context),
+                                cardWidth: cardWidth,
+                                cardHeight: cardHeight,
+                                house: state.body[index]);
+                          }),
+                      onRefresh: () async {
+                        bloc.add(InitialEvent());
+                      });
+                else
+                  return Center(child: Text('Sin casas disponibles'));
+              }
+            })));
   }
 }
